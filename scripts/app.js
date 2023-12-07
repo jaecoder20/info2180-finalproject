25893ccca1b04b912e7ae0d2d1aba26ece9ce179
Rojae Wedderburn <rojaewedderburn@gmail.com>
2023-12-06 22:57:32 -0500

    Preliminary client side validation complete - used JS alert() function to display warnings

--- scripts/app.js
+++ scripts/app.js
@@ -6,9 +6,82 @@ $(document).ready(function() {
         getDisplayAJAX(page);
         console.log("page is: ", page);
     });
-    
-});
+    //Client-Side Validation
+    $("body").on('click',".form-submit",function(event){
+        var validationFailed = false;
+        var firstname = document.querySelector('#fname');
+        var lastname = document.querySelector('#lname');
+        var telephone = document.querySelector('#telNum');
+        var email = document.querySelector('#email');
+        var password = document.querySelector('#password');
+        var company = document.querySelector('#company');
 
+        console.log("form works")
+    
+        if (isEmpty(firstname.value.trim())) {
+          validationFailed = true;
+          alert("Please fill in your First Name")
+        };
+    
+        if (isEmpty(lastname.value.trim())) {
+          validationFailed = true;
+          alert("Please fill in your Last Name")
+        }
+        if (!isValidEmail(email.value.trim())) {
+            validationFailed = true;
+            alert("Incorrect format for email address.");
+        };
+
+
+        if (password!=null){
+            if (isEmpty(password.value.trim())) {
+            validationFailed = true;
+            alert("Please fill in your Password")
+            }
+        }
+        if (company!=null){
+            if (isEmpty(company.value.trim())) {
+            validationFailed = true;
+            alert("Please fill in your Company Name")
+            }
+        }
+        if (telephone!=null){
+            if (!isValidTelephoneNumber(telephone.value.trim())) {
+            validationFailed = true;
+            alert("Incorrect format for telephone number.");
+            };
+        }
+    
+    
+        if (validationFailed) {
+          console.log('Please fix issues in Form submission and try again.');
+          element.preventDefault();
+        }
+        });
+});
+function isEmpty(elementValue) {
+    if (elementValue.length == 0) {
+      return true;
+    }
+    return false;
+  }
+  
+  /**
+   * Check if a valid telephone number was entered.
+   */
+  function isValidTelephoneNumber(telephoneNumber) {
+    return /^\d{3}-\d{3}-\d{4}$/.test(telephoneNumber);
+  }
+  
+  /**
+   * Check if a valid email address was entered.
+   */
+  function isValidEmail(emailAddress) {
+    return /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/.test(emailAddress);
+  }
+  
+  
+  
 function switchCss(page) {
     let stylesheetPath = "../styles/dashboard.css"; // Default stylesheet
 
@@ -32,8 +105,9 @@ function getDisplayAJAX(page) {
             console.error("Failed to load the page:", page);
         }
     });
-}
 
+}
+    
 function logout(){
     $.ajax({
         url: "http://localhost/info2180-finalproject/auth/logout.php",
