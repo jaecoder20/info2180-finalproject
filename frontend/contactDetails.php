0e5c6e11f3255768c1ce779def8568072e90da5c
Michael Leighton <michael138leighton@gmail.com>
2023-12-10 12:06:20 -0500

    Increase modularity

--- frontend/contactDetails.php
+++ frontend/contactDetails.php
@@ -142,37 +142,5 @@ require_once '../database/config.php';
 
 
 ?>
-<script>
-    document.getElementById('assignToMeButton').addEventListener('click', function() {
-    sendFetchRequest('assign');
-});
-
-document.getElementById('switchToSalesLeadButton').addEventListener('click', function() {
-    sendFetchRequest('switchRole');
-});
-
-function sendFetchRequest(actionType) {
-    const email = document.getElementById('info-email').textContent;
-    fetch('updateContacts.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({
-            'email': email,
-            'action': actionType
-        })
-    })
-    .then(response => response.text())
-    .then(data => {
-        alert(data);
-        window.location.reload();
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
-}
-
-
-
+<script src="../scripts/contactDetails.js">
 </script>
